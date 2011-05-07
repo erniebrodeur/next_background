7c29c0c98005f0e1cb2a5ed91dd932e814b332d7
Ernie Brodeur <ebrodeur@ujami.net>
2011-05-06 18:30:27 -0700

    Quick single wallpaper spike, should turn it into rules now.

--- next_background.rb
+++ next_background.rb
@@ -2,32 +2,24 @@
 require 'trollop'
 require 'daemons'
 
-@dir = "/home/ebrodeur/Pictures/random-directory/"
-@files = Dir.glob("#{@dir}*.jpg")
-@left_link = "/home/ebrodeur/Pictures/left"
-@right_link = "/home/ebrodeur/Pictures/right"
-@left_file = ""
-@right_file = ""
+@dir = "/home/ebrodeur/Pictures/1920x1080/"
+@files = []
+@file = ""
+@single_link = "/home/ebrodeur/Pictures/single"
 
 def get_random_file
-  file = @files[rand(@files.count)]
-  @left_file = "#{file}0"
-  @right_file = "#{file}1"
+  @files = Dir.glob("#{@dir}*")
+  @file = @files[rand(@files.count)]
 end
 
-def gen_links
-  if File.symlink?(@left_link)
-    File.delete(@left_link)
-  end
-
-  if File.symlink?(@right_link)
-    File.delete(@right_link)
+def cleanup_links
+  if File.symlink?(@single_link)
+    File.delete(@single_link)
   end
 end
 
-def fire
-  File.symlink @left_file, @left_link
-  File.symlink @right_file, @right_link
+def create_link
+  File.symlink @file, @single_link
 end
 
 def reload_desktop
@@ -37,14 +29,14 @@ end
 
 def next_background
   get_random_file
-  gen_links
-  fire
+  cleanup_links
+  create_link
   reload_desktop
 end
 
 opts = Trollop::options do
   banner <<-EOS
-This file will just fire off a few creative links in my home directories.  Since you shouldn't
+This file will just create_split_links off a few creative links in my home directories.  Since you shouldn't
 run it arbitrarily, you have to use options to make it work.
 
 next_background [options]
