ab03ed976f48a04d7b5539691b3f58c5409ff3e1
Ernie Brodeur <ebrodeur@ujami.net>
2011-05-13 10:27:15 -0700

    Added a private options hash to Daemon for some basic control (pid)

--- lib/daemon.rb
+++ lib/daemon.rb
@@ -12,16 +12,28 @@ module NextBackground
     # a reference to our parent
     attr_reader :parent
 
+    private
+    #
+    # our hash of options for daemon.  This has nothing tweakable in it at the moment.
+    attr_reader :options
+
+    #
+    # Our options hash for daemons
     #
     # our initalize requires that you feed it a parent object or it can't execute run_once.
     def initialize(parent)
       @parent = parent
+      @options = {
+        :app_name => "next_background",
+        :dir_mode => :normal,
+        :dir => "~/.cache/"
+      }
     end
 
     #
     # The entry point for this class.  
-    def fork()
-      Daemons.daemonize
+    def fork
+      Daemons.daemonize @options
       loop do
         @parent.run_once
         sleep 10
