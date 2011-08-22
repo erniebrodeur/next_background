8b8acc7c852b9295db4403d31ff24c3a64141567
Ernie Brodeur <ebrodeur@ujami.net>
2011-08-22 15:57:39 -0700

    Fixed my queries so they work, ruby doesn't copy deep methods had to add a bit extra to get it done.

--- lib/imagelist.rb
+++ lib/imagelist.rb
@@ -2,7 +2,7 @@ module NextBackground
   class FileError < StandardError
   end
 
-  class List
+  class ImageList
     # the dir we glob from
     attr_accessor :dir
     # the file mask used to glob
@@ -70,14 +70,26 @@ module NextBackground
     # 1.3 digit format
     # ie: 16:9 == 1.778
     def query_by_ratio(ratio)
-      @list.select {|k,v| v.ratio = ratio}
+      @list.select {|k,v| v.ratio == ratio}
     end
 
+    # TODO: none of this works like you think, its a reference not a copy.
+    # So when you do the following select! its pairing down the original list.
     # return the list of duplicates.
     def query_by_duplicates
-      tmp = self
-      tmp.list.select! {|k,v| v.filename.count > 1}
-      tmp
+      tmp = self.clone
+      tmp.list = list.select {|k,v| v.filename.count > 1}
+    end
+
+    # return a random file that matches the input value.
+    def randomfile_by_ratio(ratio)
+      # ruby doesn't copy deep objects on a clone, only references.
+      # So I have to restuff the trimmed down list into the new collection to use
+      # the built in methods, probably a better way to do this..
+      collection = self.clone
+      collection.list = list.select{|k,v| v.ratio == ratio }
+      # ruby trick here, I should make a 'random' object and do this right but oh well :)
+      collection.files[Random::rand(collection.files.count)]
     end
 
     # return a list of files only, as an array
