61f7095dc04940fcd422c7f6a26db19ca6894b86
Ernie Brodeur <ebrodeur@ujami.net>
2013-10-11 16:38:07 -0700

    Added jobs.

--- lib/next_background.rb
+++ lib/next_background.rb
@@ -2,4 +2,22 @@ require "next_background/background"
 require "next_background/version"
 
 module NextBackground
+  extend self
+
+  Jobs = Array.new
+
+  def add_job(background)
+    raise ArgumentError unless background.kind_of? NextBackground::Background
+
+    Jobs.push background
+  end
+
+  def run_jobs
+    Jobs.each do |job|
+      job.link_random_file
+    end
+
+    `xfdesktop --reload`
+    sleep 10
+  end
 end
