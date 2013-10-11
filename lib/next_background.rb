6108a3dba22089fdbd2103adc36094414e73b857
Ernie Brodeur <ebrodeur@ujami.net>
2013-10-11 05:01:29 -0700

    We be caching.  the random in random_file is now a class variable.

--- lib/next_background.rb
+++ lib/next_background.rb
@@ -2,5 +2,4 @@ require "next_background/background"
 require "next_background/version"
 
 module NextBackground
-
 end
--- lib/next_background/background.rb
+++ lib/next_background/background.rb
@@ -1,34 +1,45 @@
 module NextBackground
-	class Background
-		attr_accessor :link_file
-		attr_accessor :directory
+  class Background
+    attr_accessor :link_file
+    attr_accessor :directory
 
-		def initialize(params = {})
-			@link_file = params[:link_file] if params[:link_file]
-			@directory = params[:directory] if params[:directory]
-		end
+    def files
+    	@@cache ||= Bini::Sash.new options:{file:"#{Bini.cache_dir}/files.json"}
 
-		def random_file
-			random ||= Random.new
-			files ||= Dir.glob File.join(directory, "**", "*")
+    	# we don't have shit.  ain't shit to be had.
+    	if !@@cache[directory]
+    		@@cache[directory] = Dir.glob File.join(directory, "**", "*")
+    		@@cache.save
+    	end
 
-			return files[random.rand(0..files.count)]
-		end
+    	@@cache[directory]
+    end
 
-		def link_random_file
-			if File.exist?(link_file)
-				unless File.ftype(link_file) == "link"
-				raise RuntimeError, "#{link_file} does not appear to be a link."
-			else
-				FileUtils.rm link_file
-			end
-		end
+    def initialize(params = {})
+      @link_file = params[:link_file] if params[:link_file]
+      @directory = params[:directory] if params[:directory]
+    end
+
+    def random_file
+      @@random ||= Random.new
+
+      return files[@@random.rand(0..files.count)]
+    end
+
+    def link_random_file
+      if File.exist?(link_file)
+        unless File.ftype(link_file) == "link"
+          raise RuntimeError, "#{link_file} does not appear to be a link."
+        else
+          FileUtils.rm link_file
+        end
+      end
 
 
-			FileUtils.ln_s random_file, link_file
+      FileUtils.ln_s random_file, link_file
 
-			return true
-		end
+      return true
+    end
 
-	end
+  end
 end
